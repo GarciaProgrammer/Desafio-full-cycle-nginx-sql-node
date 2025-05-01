381dc8cb25e822285bceff64914e028eeb208fe3
Gabriel <gabriel.garcia@handit.com.br>
2025-04-30 22:02:17 -0300

    Desafio Nginx + node + MySQL

---
+++ node/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const port = 3000;
+
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+
+const connection = mysql.createConnection(config);
+
+const createTable = `CREATE TABLE IF NOT EXISTS people (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+)`;
+
+connection.query(createTable);
+
+app.get("/", (req, res) => {
+  const name = "Full Cycle";
+  connection.query(`INSERT INTO people(name) VALUES(?)`, [name], (err) => {
+    if (err) throw err;
+
+    connection.query(`SELECT name FROM people`, (err, results) => {
+      if (err) throw err;
+
+      const namesList = results.map((row) => `<li>${row.name}</li>`).join("");
+      res.send(`
+        <h1>Full Cycle Rocks!</h1>
+        <ul>${namesList}</ul>
+      `);
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
